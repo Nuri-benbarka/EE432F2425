0615e71cf7191bee8ca96c62fe445cf2ea4a793e
nuri-benbarka <nuribenbarka@gmail.com>
2025-01-02 16:53:43 +0200

    updated Graphs.py and added Dijikstra.py topological.py

--- Graphs.py
+++ Graphs.py
@@ -19,7 +19,7 @@ class Graph:
     def BFS(self,start=0):
         visited = set()
         visited.add(start)
-        print(start)
+        print(start,end="")
         q = []
         for i,v in enumerate(self.adjMatrix[start]):
             if v != 0 and i not in visited:
@@ -29,11 +29,22 @@ class Graph:
             if current_v in visited:
                 continue
             visited.add(current_v)
-            print(current_v)
+            print(current_v,end="")
             for i, v in enumerate(self.adjMatrix[current_v]):
                 if v != 0 and i not in visited:
                     q.append(i)
+    def DFS_rec(self,node,visited):
+        visited.add(node)
+        print(node, end="")
+        for i,n in enumerate(self.adjMatrix[node]):
+            if i in visited or n == 0:
+                continue
+            else:
+                self.DFS_rec(i,visited)
 
+    def DFS(self, start=0):
+        visited = set()
+        self.DFS_rec(start,visited)
 
 
 my_graph = Graph()
@@ -59,4 +70,6 @@ my_graph.add_edge(4,6,2)
 
 print(my_graph.adjMatrix)
 my_graph.BFS(0)
+print(" ")
+my_graph.DFS(0)
 

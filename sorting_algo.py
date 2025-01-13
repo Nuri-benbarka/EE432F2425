e5fa00de13ed173e4cb24deb77d07b66e716cde9
nuri-benbarka <nuribenbarka@gmail.com>
2025-01-13 13:50:57 +0200

    updated sorting_algo.py and added Trei.py

--- sorting_algo.py
+++ sorting_algo.py
@@ -1,5 +1,63 @@
 import random
 
+def counting_sort(arr):
+    minimum = min(arr)
+    maximum = max(arr)
+    freq = [0 for _ in range(maximum-minimum+1)]
+    for ele in arr:
+        freq[ele-minimum] += 1
+
+    result = []
+    for i , ele in enumerate(freq):
+        while ele > 0:
+            result.append(i+minimum)
+            ele -= 1
+
+    return result
+
+def radix_sort(arr):
+    freq = [[] for _ in range(10)]
+    i = 1
+    maximum = max(arr)
+    while i <= maximum:
+        while len(arr) > 0:
+            freq[(arr[0]//i)%10].append(arr.pop(0))
+        for queue in freq:
+            while len(queue) > 0:
+                arr.append(queue.pop(0))
+        i *= 10
+    return arr
+
+def quick_sort(arr):
+    if len(arr) < 2:
+        return arr
+    else:
+        pivot = arr[0]
+        smaller = [x for x in arr if x < pivot]
+        equal = [x for x in arr if x == pivot]
+        larger = [x for x in arr if x > pivot]
+        return quick_sort(larger) + equal + quick_sort(smaller)
+
+def merge_sort(arr):
+    if len(arr) < 2:
+        return arr
+    else:
+        middle_i = len(arr) // 2
+        l = merge_sort(arr[:middle_i])
+        r = merge_sort(arr[middle_i:])
+        result = []
+        while len(l) > 0 and len(r) > 0:
+            if l[0] > r[0]:
+                result.append(r.pop(0))
+            else:
+                result.append(l.pop(0))
+        while len(l) > 0:
+            result.append(l.pop(0))
+        while len(r) > 0:
+            result.append(r.pop(0))
+        return result
+
+
 
 def bubble_sort(arr):
     i = 0
@@ -67,3 +125,7 @@ print(bubble_sort([random.randint(0,100) for _ in range(10)]))
 #print(selection_sort([random.randint(0,100) for _ in range(10)]))
 print(selection_sort_2([random.randint(0,100) for _ in range(10)]))
 print(insertion_sort([random.randint(0,100) for _ in range(10)]))
+print(merge_sort([random.randint(0,100) for _ in range(10)]))
+print(quick_sort([random.randint(0,100) for _ in range(10)]))
+print(counting_sort([random.randint(0,100) for _ in range(10)]))
+print(radix_sort([random.randint(0,100) for _ in range(10)]))
